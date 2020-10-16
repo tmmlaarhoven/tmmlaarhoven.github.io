01bdde0d704d302f8e15529ca8fe613c7a916504
Thijs Laarhoven <mail@thijs.com>
2020-10-16 23:17:58 +0200

    Auto-update - 2020-10-16, 23:17:32.02

--- commands.bat
+++ commands.bat
@@ -1,7 +1,7 @@
 git pull
-REM fetchdata.py
-REM rankings.py
-REM website.py
+fetchdata.py
+rankings.py
+website.py
 git add --all --verbose
 git commit -m "Auto-update - %date%, %time%" --verbose
 git push --verbose
