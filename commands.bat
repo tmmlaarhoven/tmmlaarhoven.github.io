09ed83fdffba3936749d9eab612dd4f99dade90f
Thijs Laarhoven <mail@thijs.com>
2020-10-17 17:27:16 +0200

    Auto-update - 2020-10-17, 17:27:13.73

--- commands.bat
+++ commands.bat
@@ -1,8 +1,10 @@
-SET updmessage = "Auto-update - %date%, %time%"
+set updmessage=Auto-update - %date%, %time%
+echo %updmessage%
 git pull
 REM fetchdata.py
 REM rankings.py
 REM website.py
 git add --all --verbose
-git commit -m %updmessage% --verbose
+git commit -m "%updmessage%" --verbose
 git push --verbose
+pause 
