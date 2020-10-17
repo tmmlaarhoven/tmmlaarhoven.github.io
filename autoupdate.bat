0b7b987cb774d6dffa11f2490ae7aa64e0ac2da8
Thijs Laarhoven <mail@thijs.com>
2020-10-17 17:48:03 +0200

    Auto-update - 2020-10-17, 17:30:57.00

--- autoupdate.bat
+++ autoupdate.bat
@@ -1,8 +1,8 @@
 set updtime=%date%, %time%
 git pull
-REM fetchdata.py
-REM rankings.py
-REM website.py
+fetchdata.py
+rankings.py
+website.py
 git add --all --verbose
 git commit -m "Auto-update - %updtime%" --verbose
 git push --verbose
