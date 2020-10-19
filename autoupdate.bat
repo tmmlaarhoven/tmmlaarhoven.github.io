63e216739bf7c3ade4786e581f15d4995af94aea
Thijs Laarhoven <mail@thijs.com>
2020-10-19 20:31:35 +0200

    Auto-updater - 2020-10-19, 20:15:01.18

--- autoupdate.bat
+++ autoupdate.bat
@@ -1,3 +1,17 @@
+setlocal enabledelayedexpansion
+@ECHO OFF
+
+:wait
+set minu=%time:~3,2%
+if !minu! LEQ 15 (
+	set /a "delay=(15-!minu!)*60"
+)
+if !minu! GTR 15 (
+	set /a "delay=(75-!minu!)*60"
+)
+timeout !delay!
+
+:loop
 set updtime=%date%, %time%
 git pull
 fetchdata.py
@@ -6,3 +20,4 @@ website.py
 git add --all --verbose
 git commit -m "Auto-updater - %updtime%" --verbose
 git push --verbose
+goto wait
