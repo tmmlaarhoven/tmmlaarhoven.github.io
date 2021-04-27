06cdffade6a1d23239e045e7be389d7ea1817097
Thijs Laarhoven <mail@thijs.com>
2021-04-27 18:25:43 +0200

    manual plot update

--- Caller.py
+++ Caller.py
@@ -5,15 +5,28 @@ import time
 import collections
 import os.path
 import ndjson
+import shutil
 from Utilities import *
+
+DrawPlots = False
 
 Cat = dict()
 for V in AllVariants:
 	Cat[V] = dict()
 	for E in AllEvents:
-		Cat[V][E] = ArenaCategory(V, E)
-		Cat[V][E].LoadRankings()
-		Cat[V][E].UpdateRankings()
-		Cat[V][E].UpdatePlots()
-		Cat[V][E].UpdateWebsite()
-		del Cat[V][E]
+
+		if DrawPlots:
+			shutil.rmtree(f"E:\\lichess\\tournaments\\rankings\\{V}\\{E}\\players\\")			
+			Cat[V][E] = ArenaCategory(V, E)
+			Cat[V][E].LoadRankings()
+			Cat[V][E].UpdateRankings()
+			Cat[V][E].UpdatePlots()
+			Cat[V][E].UpdateWebsite()
+			del Cat[V][E]
+		else:
+			Cat[V][E] = ArenaCategory(V, E)
+			Cat[V][E].LoadRankings()
+			Cat[V][E].UpdateRankings()
+			Cat[V][E].UpdateWebsite()
+			del Cat[V][E]
+			
