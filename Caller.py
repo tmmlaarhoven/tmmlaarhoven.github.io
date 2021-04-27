fa02d7414f84cd6128168a4988a74924acf60a30
Thijs Laarhoven <mail@thijs.com>
2021-04-27 04:11:47 +0200

    Auto-updater - 2021-04-27,  3:16:52.68

--- Caller.py
+++ Caller.py
@@ -14,6 +14,6 @@ for V in AllVariants:
 		Cat[V][E] = ArenaCategory(V, E)
 		Cat[V][E].LoadRankings()
 		Cat[V][E].UpdateRankings()
-		#Cat[V][E].UpdatePlots()
+		Cat[V][E].UpdatePlots()
 		Cat[V][E].UpdateWebsite()
 		del Cat[V][E]
